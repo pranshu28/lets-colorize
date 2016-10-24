afc86d546cc42277a9ee68f1a3126e1e71b22206
Pranshu Malviya <pranshumalviya2@users.noreply.github.com>
2016-10-25 03:51:39 +0530

    Update colorize.py

--- convert/colorize.py
+++ convert/colorize.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pandas as pd
 import cv2
-from gco_python import pygco
+import pygco
+
+import warnings
+warnings.filterwarnings("ignore")
 
 def get_edges(img, blur_width=3):
 	img_blurred = cv2.GaussianBlur(img, (0, 0), blur_width)
@@ -11,15 +14,13 @@ def get_edges(img, blur_width=3):
 
 def graphcut(img,edges,label_costs, l=100):
 	num_classes = len(colors)
-    #calculate pariwise potiential costs (distance between color classes)
 	pairwise_costs = np.zeros((num_classes, num_classes))
 	for ii in range(num_classes):
 		for jj in range(num_classes):
-			c1 = np.array(colors[1:,ii])
-			c2 = np.array(colors[1:,jj])
+			c1 = np.array(colors[ii,1:])
+			c2 = np.array(colors[jj,1:])
 			pairwise_costs[ii,jj] = np.linalg.norm(c1-c2)
-    
-	label_costs_int32 = (100*label_costs).astype('int32')
+	label_costs_int32 = np.ascontiguousarray(label_costs).astype('int32')
 	pairwise_costs_int32 = (l*pairwise_costs).astype('int32')
 	vv_int32 = edges.astype('int32')
 	vh_int32 = edges.astype('int32')
@@ -31,21 +32,30 @@ test = cv2.imread('img_8.jpg',0)
 df = pd.read_csv('pred.csv', sep=',')
 dfpr = pd.read_csv('pred_prob.csv', sep=',')
 colors = pd.read_csv('colors.csv', sep=',',header=None).as_matrix()
-pixels = df.ix[:,1:3].as_matrix()
-pred = df.ix[:,3:].as_matrix()
-pred_prob = dfpr.ix[:,3:].as_matrix()
-edges = get_edges(img)
-output_labels = graphcut(test,edges,pred_prob, l=1)
+pixels = df.ix[:,:2].as_matrix()
+pred = df.ix[:,2:].as_matrix()
+pred_prob = dfpr.ix[:,2:].as_matrix()
+
+rows, cols = test.shape
+label_costs = np.zeros((rows,cols,len(colors)))
+for i,x in enumerate(pixels):
+	cost = -100*pred_prob[i]
+	label_costs[x[0],x[1]] = np.array(cost).astype(int)
+edges = get_edges(test)
+output_labels = graphcut(test,edges,label_costs, l=1)
 
 #Colorization
+output_a = np.zeros((rows,cols))
+output_b = np.zeros((rows,cols))
 for i in pixels[:,0]:
 	for j in pixels[:,1]:
 		a,b = colors[output_labels[i,j],1:]
 		output_a[i,j] = a
 		output_b[i,j] = b
 
-output_img = cv2.cvtColor(cv2.merge((img, np.uint8(output_a), np.uint8(output_b))), cv2.CV_Lab2RGB)
+output_img = cv2.cvtColor(cv2.merge((test, np.uint8(output_a), np.uint8(output_b))), cv2.COLOR_Lab2RGB)
 
 cv2.imshow('Test',output_img)
+cv2.imwrite('RESULT.jpg',output_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
