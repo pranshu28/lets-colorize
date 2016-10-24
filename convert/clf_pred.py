cec74af12ceabf37635e2f7442c3e41c68c9f486
Pranshu Malviya <pranshumalviya2@users.noreply.github.com>
2016-10-25 03:50:26 +0530

    Update clf_pred.py

--- convert/clf_pred.py
+++ convert/clf_pred.py
@@ -6,25 +6,28 @@ from sklearn.svm import SVC
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.multiclass import OneVsRestClassifier
 
-reg = 1.0							#Regularization in SVMs
-
+reg = 1.5							#Regularization in SVMs
 
 #Classification
-df = pd.read_csv('ftr_ext_train.csv', sep=',')
-train_svc = df.ix[:,1:4].as_matrix()
-pca_ftr = df.ix[:,4:].as_matrix()
+df = pd.read_csv('ftr_ext_train.csv', sep=',',header=None)
+train_svc = df.ix[:,1:2].as_matrix()
+colors = np.vstack({tuple(row) for row in train_svc})
+labels=[]
+for i,col in enumerate(train_svc):
+	lab = np.where(np.all(colors==col,axis=1))
+	labels.append(lab[0])
+pca_ftr = df.ix[:,3:].as_matrix()
 start = timeit.default_timer()
-Y = MultiLabelBinarizer().fit_transform(train_svc[:,1:])
+Y = MultiLabelBinarizer().fit_transform(labels)
 clf = OneVsRestClassifier(SVC(C=reg,kernel='linear',probability=True))
 clf.fit(pca_ftr, Y)
 stop = timeit.default_timer()
 print ("Train - Classification: Done in ",stop-start," sec - Feature: ",pca_ftr.shape,"	Data: ",Y.shape)
 
-
 #Predict
-df = pd.read_csv('ftr_ext_test.csv', sep=',')
-pixels = df.ix[:,1:3].as_matrix()
-pca_ftr_test = df.ix[:,3:].as_matrix()
+df = pd.read_csv('ftr_ext_test.csv', sep=',',header=None)
+pixels = df.ix[:,:1].as_matrix()
+pca_ftr_test = df.ix[:,2:].as_matrix()
 start = timeit.default_timer()
 predict = np.matrix(clf.predict(pca_ftr_test))
 predict_prob = np.matrix(clf.predict_proba(pca_ftr_test))
@@ -32,7 +35,8 @@ stop = timeit.default_timer()
 print ("Test - Prediction: Done in ",stop-start," sec - Test: ",pca_ftr_test.shape,"	Result: ",predict.shape,predict_prob.shape)
 
 df = pd.DataFrame(np.concatenate((pixels, predict), 1))
-df.to_csv('pred.csv', sep=',')
+df.to_csv('pred.csv', sep=',',header=False,index=False)
+pd.DataFrame(colors).to_csv('colors.csv', sep=',',header=None)
 
 dfpr = pd.DataFrame(np.concatenate((pixels, predict_prob), 1))
-dfpr.to_csv('pred_prob.csv', sep=',')
+dfpr.to_csv('pred_prob.csv', sep=',',header=False,index=False)
